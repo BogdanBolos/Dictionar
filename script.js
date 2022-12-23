3b47c48de7408b987eb6c292d7a39def37d58e5b
BogdanBolos <119728025+BogdanBolos@users.noreply.github.com>
2022-12-23 16:24:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 document.getElementById("add").onclick = function() {
     var text = document.getElementById("input").value;
     var li = "<li>" + text + "</li>";
-    if (checkWord(li) == 1) {
+    if (checkWord(text) == 1) {
         document.getElementById("list").innerHTML += li;
         document.getElementById("input").value = "";
+    } else {
+        alert("This word already exists in the list");
+        document.getElementById("input").value = "";
     }
+}
+
+function checkWord(text) {
+    var word = document.getElementById("list");
+    var wordListLi = word.getElementsByTagName("li");
+    for (var i = 0; i < wordListLi.length; i++) {
+        if (text === wordListLi[i].textContent) {
+           return 0;
+        }
+    }
+    return 1;
 }
